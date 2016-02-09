00f39c79608d0c4bf4527f2fbd6d037fe757762d
ashersz <asher@curiyo.com>
2016-02-09 10:21:50 +0200

    added meteor version in case no meteor app

--- base/scripts/run_app.sh
+++ base/scripts/run_app.sh
@@ -16,6 +16,7 @@ elif [[ $BUNDLE_URL ]]; then
 elif [ -d /built_app ]; then
   cd /built_app
 else
+  echo "=> meteor version:`meteor --version` "
   echo "=> You don't have an meteor app to run in this image."
   exit 1
 fi
