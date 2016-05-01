43f698e5c5f1da9dcdf24bae8e5fb6e2a3d9c3d6
ashersz <asher@curiyo.com>
2016-05-01 13:14:47 +0300

    added echo to debug

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -1,5 +1,5 @@
 set -e
-
+echo "starting build_app"
 COPIED_APP_PATH=/copied-app
 BUNDLE_DIR=/tmp/bundle-dir
 
@@ -7,12 +7,12 @@ BUNDLE_DIR=/tmp/bundle-dir
 # this fixes that
 cp -R /app $COPIED_APP_PATH
 cd $COPIED_APP_PATH
-
+echo "app copied to $COPIED_APP_PATH"
 meteor build --directory $BUNDLE_DIR --server=http://localhost:3000
-
+echo "after meteor build"
 cd $BUNDLE_DIR/bundle/programs/server/
 npm i
-
+echo " after npm install"
 mv $BUNDLE_DIR/bundle /builtapp
 
 # cleanup
