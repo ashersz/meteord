719e59dd4cb308ed35ee20abc524f6643cd5072b
ashersz <asher@curiyo.com>
2016-06-02 11:16:16 +0300

    added npm install on client side

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -8,11 +8,21 @@ BUNDLE_DIR=/tmp/bundle-dir
 cp -R /app $COPIED_APP_PATH
 cd $COPIED_APP_PATH
 echo "app copied to $COPIED_APP_PATH"
+npm install && npm cache clear
+echo "after npm install on client side"
 meteor build --server-only --directory $BUNDLE_DIR --server=http://localhost:3000
 echo "after meteor build"
 cd $BUNDLE_DIR/bundle/programs/server/
-npm i
-echo " after npm install"
+npm install && npm cache clear
+echo " after npm install server side"
+if [ -d "$COPIED_APP_PATH/node_modules" ];then
+  echo "copy client node_modules to web.browser"
+  cp -R $COPIED_APP_PATH/node_modules $BUNDLE_DIR/bundle/programs/web.browser
+  if [ -d "$BUNDLE_DIR/bundle/programs/web.cordova" ];then
+      echo "copy client node_modules to web.cordova"
+      cp -R $COPIED_APP_PATH/node_modules $BUNDLE_DIR/bundle/programs/web.cordova
+  fi
+fi
 mv $BUNDLE_DIR/bundle /built_app
 touch /var/log/meteor.log
 
