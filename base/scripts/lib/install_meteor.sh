757e03d73a61cf77658fb321f2c52bd05c60879b
ashersz <asher@curiyo.com>
2016-10-27 15:26:04 +0300

    use meteor 1.4.0.1

--- base/scripts/lib/install_meteor.sh
+++ base/scripts/lib/install_meteor.sh
@@ -1,2 +1,4 @@
 set -e
-curl -sL https://install.meteor.com | sed s/--progress-bar/-sL/g | /bin/sh
+export METEOR_NO_RELEASE_CHECK=true # make sure this is set in your environment.
+# curl -sL https://install.meteor.com | sed s/--progress-bar/-sL/g | /bin/sh
+curl https://install.meteor.com/?release=1.4.0.1 | sh # You can use the exact version you're deploying here
