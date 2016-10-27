5d3029cbc4e20fcbeedab5eeffd9c91bab14e2a0
ashersz <asher@curiyo.com>
2016-10-27 15:22:41 +0300

    Revert "added --unsafe-perm in echo"
    This reverts commit f958762a3f013d85fc4c27fc70441181f406687b.

--- base/scripts/lib/build_app.sh
+++ base/scripts/lib/build_app.sh
@@ -16,9 +16,9 @@ if [ -f "$COPIED_APP_PATH/package.json" ];then
   npm install && npm cache clear
   echo "finished install on client side"
 fi
-echo "before meteor build with --unsafe-perm"
+echo "before meteor build"
 meteor build --unsafe-perm --server-only --directory $BUNDLE_DIR --server=http://localhost:3000
-echo "after meteor build with --unsafe-perm"
+echo "after meteor build"
 cd $BUNDLE_DIR/bundle/programs/server/
 echo "before npm install server side"
 npm install && npm cache clear
