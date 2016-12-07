d7f469b0ef1f53e7a7b87e0472c47ba2ed1e850d
ashersz <asher@curiyo.com>
2016-12-07 10:30:28 +0200

    installing the version of meteor according to .meteor/release

--- base/scripts/lib/install_meteor.sh
+++ base/scripts/lib/install_meteor.sh
@@ -1,4 +1,6 @@
 set -e
 export METEOR_NO_RELEASE_CHECK=true # make sure this is set in your environment.
 # curl -sL https://install.meteor.com | sed s/--progress-bar/-sL/g | /bin/sh
-curl https://install.meteor.com/?release=1.4.0.1 | sh # You can use the exact version you're deploying here
+# read in the release version in the app
+METEOR_VERSION=$(head /app/.meteor/release | cut -d "@" -f 2)
+curl https://install.meteor.com/?release=$METEOR_VERSION | sed s/--progress-bar/-sL/g | /bin/sh # You can use the exact version you're deploying here
